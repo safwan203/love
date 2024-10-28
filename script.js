fd4e3a48dd08f69d4b52c528ce245de336e1fa79
safwan203 <safwansajjad15@gmail.com>
2024-10-28 10:14:29 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// function add(){
+//     let num1=10
+//     let num2=40
+//     let result=num1+num2
+//     alert(result)
+// }
+
+
+let name1 = prompt('enter your Name')
+let name2 = prompt('enter your partner Name')
+
+let num =Math.floor(Math.random() * 100) +1
+alert(num)
+// console.log(num);
+if(num > 70){
+     alert("your love score is "+num +" % and you are made for aech other")
+}
+else{
+    alert("your love score is "+num +" % and change your partner")
+}
